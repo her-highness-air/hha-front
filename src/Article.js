2b31950512279f83999d770b6d1bfb48a3a09386
Vivek Anand <i0744@IT000736.local>
2019-08-27 11:58:58 +0530

    Add initial boilerplate code
    Signed-off-by: Vivek Anand <i0744@IT000736.local>

---
+++ src/Article.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="article">
+      <header className="article-header">
+      </header>
+      <body>
+          <p>Full article here</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ArticlePreview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function ArticlePreview() {
+  return (
+    <div className="article">
+      <header className="article-header">
+        <p class="article-para">
+            Something will go here.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default ArticlePreview;
